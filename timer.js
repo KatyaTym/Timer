f3efa968e73bf216653f03b95614c52c39bc786a
KatyaTym <palengakatusa01@gmail.com>
2019-07-20 14:38:58 +0300

    Add countdown

--- timer.js
+++ timer.js
@@ -5,10 +5,10 @@ let timer_button_state = true; // Start == true, Stop == false
 function timer_button_tumbler() {
     timer_button_state = !timer_button_state;
     if (timer_button_state) {
-        timer_button.textContent = "Start";
+        timer_button.value = "Start";
         clearInterval(interval);
     } else {
-        timer_button.textContent = "Stop";
+        timer_button.value = "Stop";
         timer(5);
     }
 };
@@ -21,6 +21,7 @@ function timer(time) {
         if (line) {
             line.setAttribute("style", 'width:' + a + '%');
         }
+        setCountdownValue(time);
         --time;
         if (time < 0) {
             timer_button_tumbler();
@@ -28,4 +29,20 @@ function timer(time) {
     };
     timer_tick();
     interval = setInterval(timer_tick, 1000);
+}
+
+function setCountdownValue (time) {
+    const countdown = document.getElementById('countdown');
+    const minutes = countdown.getElementsByClassName('minutes') [0];
+    const seconds = countdown.getElementsByClassName('seconds') [0];
+    let m = Math.floor (time / 60);
+    let s = time - m * 60;
+    if (m < 10) {
+        m = '0' + m;
+    }
+    if (s < 10) {
+        s = '0' + s;
+    }
+    minutes.textContent = m;
+    seconds.textContent = s;
 }
