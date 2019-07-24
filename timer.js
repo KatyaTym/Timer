d5f1eda8b5b7b9814e78b1d5af7cb1ab1d0c865f
KatyaTym <palengakatusa01@gmail.com>
2019-07-24 17:12:07 +0300

    Create timer via js

--- timer.js
+++ timer.js
@@ -1,17 +1,59 @@
 let interval;
-const timer_button = document.getElementById("timer_button");
 
 let timer_button_state = true; // Start == true, Stop == false
 function timer_button_tumbler() {
     timer_button_state = !timer_button_state;
     if (timer_button_state) {
-        timer_button.value = "Start";
+        getTimerButton("timer1").value = "Start";
         clearInterval(interval);
     } else {
-        timer_button.value = "Stop";
+        getTimerButton("timer1").value = "Stop";
         timer(5, 1000);
     }
-};
+}
+
+function createTimer(timer_id) {
+    const timer = document.createElement("div");
+    timer.id = timer_id;
+
+    const countdown = document.createElement("div");
+    countdown.classList.add("countdown");
+    countdown.textContent = "00:00";
+    timer.appendChild(countdown);
+
+    const button = document.createElement("input");
+    button.type = "button";
+    button.classList.add("timer_button");
+    button.onclick = timer_button_tumbler;
+    button.value = "Start";
+    timer.appendChild(button);
+
+    const line = document.createElement("div");
+    line.classList.add("timer_line");
+    timer.appendChild(line);
+
+    document.getElementById("timers").appendChild(timer);
+}
+
+function getCountdown(timer_id) {
+    const timer = document.getElementById(timer_id);
+    const countdown = timer.getElementsByClassName("countdown")[0];
+    return countdown;
+}
+
+function getTimerButton(timer_id) {
+    const timer = document.getElementById(timer_id);
+    const timer_button = timer.getElementsByClassName("timer_button")[0];
+    return timer_button;
+}
+
+function getTimerLine(timer_id) {
+    const timer = document.getElementById(timer_id);
+    const timer_line = timer.getElementsByClassName("timer_line")[0];
+    return timer_line;
+}
+
+createTimer("timer1");
 
 function timer(time, span) {
     const startTime = time;
@@ -28,14 +70,12 @@ function timer(time, span) {
 };
 
 function setTimerLineWidth(width) {
-    const line = document.getElementById('timer_line');
+    const line = getTimerLine("timer1");
     line.setAttribute("style", 'width:' + width + '%');
 };
 
 function setCountdownValue(time) {
-    const countdown = document.getElementById('countdown');
-    const minutes = countdown.getElementsByClassName('minutes')[0];
-    const seconds = countdown.getElementsByClassName('seconds')[0];
+    const countdown = getCountdown("timer1");
     let m = Math.floor(time / 60);
     let s = time - m * 60;
     if (m < 10) {
@@ -44,6 +84,5 @@ function setCountdownValue(time) {
     if (s < 10) {
         s = '0' + s;
     }
-    minutes.textContent = m;
-    seconds.textContent = s;
+    countdown.textContent = m + ":" + s;
 };
