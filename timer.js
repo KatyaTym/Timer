2defa8506f1111e7413dcef5fbc40c6aeb58c475
KatyaTym <palengakatusa01@gmail.com>
2019-07-24 19:49:04 +0300

    Reorganize to class

--- timer.js
+++ timer.js
@@ -1,88 +1,81 @@
-let interval;
+class Timer {
+    constructor(time, span, auto) {
+        this.time = time;
+        this.start_time = time;
+        this.span = span;
+        this.started = false;
 
-let timer_button_state = true; // Start == true, Stop == false
-function timer_button_tumbler() {
-    timer_button_state = !timer_button_state;
-    if (timer_button_state) {
-        getTimerButton("timer1").value = "Start";
-        clearInterval(interval);
-    } else {
-        getTimerButton("timer1").value = "Stop";
-        timer(5, 1000);
-    }
-}
+        this.timer = document.createElement("div");
 
-function createTimer(timer_id) {
-    const timer = document.createElement("div");
-    timer.id = timer_id;
+        this.countdown = document.createElement("div");
+        this.countdown.classList.add("countdown");
+        this.setCountdownValue();
+        this.timer.appendChild(this.countdown);
 
-    const countdown = document.createElement("div");
-    countdown.classList.add("countdown");
-    countdown.textContent = "00:00";
-    timer.appendChild(countdown);
+        this.button = document.createElement("input");
+        this.button.type = "button";
+        this.button.classList.add("timer_button");
+        this.button.onclick = this.onClick.bind(this);
+        this.button.value = "Start";
+        this.timer.appendChild(this.button);
 
-    const button = document.createElement("input");
-    button.type = "button";
-    button.classList.add("timer_button");
-    button.onclick = timer_button_tumbler;
-    button.value = "Start";
-    timer.appendChild(button);
+        this.line = document.createElement("div");
+        this.line.classList.add("timer_line");
+        this.timer.appendChild(this.line);
 
-    const line = document.createElement("div");
-    line.classList.add("timer_line");
-    timer.appendChild(line);
+        document.body.appendChild(this.timer);
 
-    document.getElementById("timers").appendChild(timer);
-}
-
-function getCountdown(timer_id) {
-    const timer = document.getElementById(timer_id);
-    const countdown = timer.getElementsByClassName("countdown")[0];
-    return countdown;
-}
-
-function getTimerButton(timer_id) {
-    const timer = document.getElementById(timer_id);
-    const timer_button = timer.getElementsByClassName("timer_button")[0];
-    return timer_button;
-}
-
-function getTimerLine(timer_id) {
-    const timer = document.getElementById(timer_id);
-    const timer_line = timer.getElementsByClassName("timer_line")[0];
-    return timer_line;
-}
-
-createTimer("timer1");
-
-function timer(time, span) {
-    const startTime = time;
-    const timer_tick = function() {
-        setTimerLineWidth(time / startTime * 100);
-        setCountdownValue(time);
-        --time;
-        if (time < 0) {
-            timer_button_tumbler();
+        if (auto) {
+            this.start();
         }
-    };
-    timer_tick();
-    interval = setInterval(timer_tick, span);
-};
-
-function setTimerLineWidth(width) {
-    const line = getTimerLine("timer1");
-    line.setAttribute("style", 'width:' + width + '%');
-};
-
-function setCountdownValue(time) {
-    const countdown = getCountdown("timer1");
-    let m = Math.floor(time / 60);
-    let s = time - m * 60;
-    if (m < 10) {
-        m = '0' + m;
     }
-    if (s < 10) {
-        s = '0' + s;
+
+    setCountdownValue() {
+        let m = Math.floor(this.time / 60);
+        let s = this.time - m * 60;
+        if (m < 10) {
+            m = '0' + m;
+        }
+        if (s < 10) {
+            s = '0' + s;
+        }
+        this.countdown.textContent = m + ":" + s;
     }
-    countdown.textContent = m + ":" + s;
-};
+
+    timerTick() {
+        const width = this.time / this.start_time * 100;
+        this.line.setAttribute("style", 'width:' + width + '%');
+        this.setCountdownValue();
+        if (this.time <= 0) {
+            this.stop();
+        } else {
+            this.time = this.time - this.span / 1000;
+        }
+    }
+
+    start() {
+        this.started = true;
+        this.button.value = "Stop";
+
+        this.timerTick();
+        this.interval = setInterval(this.timerTick.bind(this), this.span);
+    }
+
+    onClick() {
+        if (this.started) {
+            this.stop();
+        } else {
+            this.start();
+        }
+    }
+
+    stop() {
+        this.started = false;
+        this.button.value = "Start";
+
+        clearInterval(this.interval);
+    }
+}
+
+const t1 = new Timer(1 * 60, 1000, false);
+const t2 = new Timer(99 * 60, 2000, true);
